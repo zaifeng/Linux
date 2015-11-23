a80373922c37eb215becba42b0664ff7d7f07aa6
zaifeng <20012382@qq.com>
2015-11-23 09:23:40 +0800

    update math calculate

--- shell/forin.sh
+++ shell/forin.sh
@@ -3,3 +3,13 @@ for state in apple orange pear banana pineapple
 do
     echo "$state is my favourite fruit"
 done | sort
+
+sum=0
+for ((i=1 ; i <11;i++))
+{
+    #((sum=$sum+$i)) 
+    #let sum=sum+i
+    sum=`expr $sum + $i`
+}
+
+echo $sum;
