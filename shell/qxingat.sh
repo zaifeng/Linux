e739674241a45b9a68578071cc92c3b810e5a935
zaifeng <20012382@qq.com>
2016-01-24 12:34:15 +0800

    update practise

--- shell/qxingat.sh
+++ shell/qxingat.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # $* 与 $@区别就是当被双引号括起来的时候，$*是单一字符串，$@是多个成员字符串
+echo "\$* 输出"
 for a in "$*"
 do
     echo $a
-    echo "\$* 输出"
 done
 
+echo "\$@输出"
 for b in "$@"
 do
     echo $b
-    echo "\$@输出"
 done
