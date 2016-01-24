9dc02b5e239af4336fbca689170663b87ee38718
zaifeng <20012382@qq.com>
2016-01-24 13:14:38 +0800

    update practise

--- shell/func.sh
+++ shell/func.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-function myfunc()
+restartNginx()
 {
-   echo abc 
+    echo Stopping Nginx...
+    sleep 2
+    echo Starting Nginx    
 }
 
-echo $@
-echo ===============
+restartPhpfpm()
+{
+    echo Stopping PHP-fpm...
+    sleep 2
+    echo Starting PHP-fpm
+}
 
-echo $*
+usage()
+{
+    echo 'Example:func.sh [php/nginx] [start|stop|restart]'
+}
+
+if [ $# -ne 2 ] ;then
+    echo Please input 2 parameters
+    exit  
+fi
+
+case $1 in
+    "php")
+        restartPhpfpm
+        echo "12"
+    ;;
+    "nginx")
+        restartNginx
+    ;;
+    *)
+        usage
+        ;;
+esac
+    
+
+
+
