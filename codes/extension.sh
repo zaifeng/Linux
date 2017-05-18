ee9e3e14b5155b9b6b00e07c08d8113393172fe2
zaifeng <20012382@qq.com>
2017-05-18 14:34:57 +0800

    update shell example

--- codes/extension.sh
+++ codes/extension.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-filename=www.filejpg.pkg
-file_ext=${filename%.*}
-file_ext1=${filename%%.*}
-file_pre=${filename#*.}
-file_pre1=${filename##*.}
-echo Filename:$file_ext FilePrefix :$file_pre
-echo Filename:$file_ext1 FilePrefix :$file_pre1
+filename=www-1.file-jpg.pkg
+file_ext=${filename%-*}
+file_ext1=${filename%%-*}
+file_pre=${filename#*-}
+file_pre1=${filename##*-}
 
+echo 'filename=' $filename
+echo nongreedy trim right
+echo '${filename%-*}=' $file_ext
+echo
+echo greedy trim right
+echo '${filename%%-*}=' $file_ext1
+echo 
+echo nongreedy trim left
+echo '${filename#*-}=' $file_pre
+echo
+echo greedy trim left
+echo '${filename##*-}=' $file_pre1
+
+jpg=hello.world.jpg
+
+echo file_ext '=' ${jpg##*.}
+echo file_name '=' ${jpg%.*}
 
 
